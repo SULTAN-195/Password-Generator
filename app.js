7f6d2a4d744651f965eccb2fa67a145c5e64d4fa
SULTAN-195 <a.sultan.syed195@gmail.com>
2025-07-15 19:48:56 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,96 +1,52 @@
-// --------------------------------------------- Password Generator Project -----------------------------------------------
-
-
-let slider = document.getElementById("lengthInput");
-
-let display = document.getElementById("lengthValue");
-
-slider.addEventListener("input", function (){
-    display.innerText = slider.value;
-})
+function addTask() {
+    let input = document.getElementById("input-Task");
+    let taskText = input.value.trim();
 
 
 
 
-function GeneratePassword() {
-    
-    let slider0 = document.getElementById("lengthInput").value;
-
-    let upper0 = document.getElementById("upperCase").checked;
-
-    let lower0 = document.getElementById("lowerCase").checked;
-
-    let number0 = document.getElementById("number").checked;
-
-    let symbol0 = document.getElementById("symbol").checked;
-
-    
-    
-    let chars = "";
-    
-    
-    
-    
-    let upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lowerSet = "abcdefghijklmnopqrstuvwxyz";
-    let numberSet = "0123456789";
-    let symbolSet = "!@#$%^&*()_+=?}|?:<>/";
-    
-    
-    
-    
-    
-    if (upper0) {
-        chars += upperSet;
-    }
-    
-    if (lower0) {
-        chars += lowerSet;
-    }
-    
-    if (number0) {
-        chars += numberSet;
-    }
-    
-    
-    if (symbol0) {
-        chars += symbolSet;
-    }
-    
-    
-    
-    
-    if (chars === "") {
-        alert("Please select at least one character type!");
-        return
+    if (taskText === "") {
+        alert("Please Enter a Task!")
+        return;
     }
 
 
 
 
-    let password1 = "";
+    let taskList = document.getElementById("taskList");
+    let newTask = document.createElement("li")
+    newTask.textContent = taskText
 
 
 
-    for (let i = 0; i < slider0; i++) {
-        let randIndex = Math.floor(Math.random() * chars.length);
-        password1 += chars[randIndex];
-    }
 
 
-    document.getElementById("passwordOutput").value = password1;
+
+    var deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+
+    deleteBtn.onclick = function () {
+    newTask.remove();
+    };
+
+
+
+
+
+
+    newTask.onclick = function () {
+        newTask.style.textDecoration = "line-through";
+    };
+
+
+
+
+
+    taskList.appendChild(newTask);
+    newTask.appendChild(deleteBtn);
+
+
+    input.value = "";
+
+
 }
-
-
-document.getElementById("CopyPassword").addEventListener("click", function() {
-    
-    let text = document.getElementById("passwordOutput").value;
-
-    navigator.clipboard.writeText(text)
-    .then(() => {
-        alert("Copied Successfully!")
-    })
-    .catch(() => {
-        alert("Copied Failed! Try Again")
-    })
-})
